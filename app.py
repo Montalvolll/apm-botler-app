0ee5d1e59ce492a90ccbc66590b5f61d05589eaf
MONthinkpad <jmontalvo@sanservices.hn>
2022-08-11 10:35:47 -0600

    more identation changes

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import hashlib
 import hmac
 from flask import Flask, request
@@ -5,29 +7,36 @@ from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, World!"
 
-@app.route("/sms", methods=['POST'])
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+
+@app.route('/sms', methods=['POST'])
 def reply():
 
     secret = 'qn7VoYDD6imwdFjjRmK0SexbRsgjpGoOgoboYHOjVqQ='
     encoded_secret = str.encode(secret)
     msg = request.form.get('Body')
 
-    hashed = 'HMAC ' + hmac.new(encoded_secret, msg.encode('UTF-8'), hashlib.sha256).digest()
+    hashed = 'HMAC ' + hmac.new(encoded_secret, msg.encode('UTF-8'),
+                                hashlib.sha256).digest()
+
     # digested_value = body.digest()
 
     # if(msg.lower() == "hello"):
-    if(hashed == request.headers.get('authorization')): #not yet finished...
-    	if(msg == 'Hola'):
-            reply = "Hi, this is an automated reply." 
+
+    if hashed == request.headers.get('authorization'):  # not yet finished...
+        if msg == 'Hola':
+            reply = 'Hi, this is an automated reply.'
+        elif(msg == 'APM'):
+            reply = 'Best of Bests'
         else:
-            reply = "Command not found."
+            reply = 'Command not found.'
     response = MessagingResponse()
     response.message(reply)
     return str(response)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
